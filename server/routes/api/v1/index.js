b475f3c116053286391dada56a6c8293ac513079
herbert <herbixbix@gmail.com>
2018-11-20 08:31:46 -0800

    file updated

--- server/routes/api/v1/index.js
+++ server/routes/api/v1/index.js
@@ -4,6 +4,10 @@ import parcelRouters from "./parcel";
 
 const routers = Router();
 
-routers.use(userRouters, parcelRouters);
+const notFound = (req, res) => {
+  return res.status(404).send({message:"the url not found"})
+}
+
+routers.use(userRouters, parcelRouters,notFound);
 
 export default routers;
